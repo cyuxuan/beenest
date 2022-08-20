e1c0a41a1543877007e50cc23f6bfd5c2c49d140
Cyuxuan <cyuxuan@163.com>
2022-08-21 00:06:10 +0800

    初始化网关项目

---
+++ bee-infrastructure/bee-gateway/bee-spring-cloud-gateway/src/main/java/club/beenest/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package club.beenest.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 陈玉轩
+ * @since 2022-08-20
+ */
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
