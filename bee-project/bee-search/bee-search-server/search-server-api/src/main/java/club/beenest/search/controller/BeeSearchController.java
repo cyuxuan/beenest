f25f078535e669ebf3f8ef20378e003faf225789
Cyuxuan <cyuxuan@163.com>
2022-11-03 22:47:02 +0800

    前端项目初始化

---
+++ bee-project/bee-search/bee-search-server/search-server-api/src/main/java/club/beenest/search/controller/BeeSearchController.java
@@ -0,0 +1,25 @@
+package club.beenest.search.controller.beeseaarch;
+
+import club.beenest.search.BeeSearchService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class BeeSearchController {
+    @Resource
+    private BeeSearchService beeSearchService;
+
+    /**
+     * 搜索主接口
+     *
+     * @return 返回搜索结果
+     */
+    @PostMapping("/search")
+    @ResponseBody
+    public String doSearch() {
+        return String.valueOf(beeSearchService.doSearch());
+    }
+}
--- bee-project/bee-search/bee-search-server/search-server-boot/src/main/java/club/beenest/search/SearchApplication.java
+++ bee-project/bee-search/bee-search-server/search-server-boot/src/main/java/club/beenest/search/SearchApplication.java
@@ -1,5 +1,6 @@
 package club.beenest.search;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@MapperScan("club.beenest.search.dao")
 public class SearchApplication {
     public static void main(String[] args) {
         SpringApplication.run(SearchApplication.class);
